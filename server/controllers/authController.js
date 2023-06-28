766da2b87633a5b8a49bd25e1f73f257366cb5a6
Egor Work <e.moisei1@ggg.com>
2023-06-28 16:26:27 +0300

    finish register method

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,25 +1,41 @@
-import User from '../models/User.js';
+import User from "../models/User.js";
 
 //Register
 
 export const register = async (req, res) => {
-    try {
-        const { username, password } = req.body;
-        const isUsed = await User.findOne({ username });
-        if (isUsed) {
-            return res.json({
-                message: 'Данный username уже занят',
-            });
-        }
-    } catch (e) {}
+  try {
+    const { username, password } = req.body;
+    const isUsed = await User.findOne({ username });
+    if (isUsed) {
+      return res.json({
+        message: "Данный username уже занят",
+      });
+    }
+
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+
+    const newUser = new User({
+      username,
+      password: hash,
+    });
+
+    await newUser.save();
+    return res.json({
+      newUser,
+      message: "Пользователь успешно зарегистрирован",
+    });
+  } catch (e) {
+    res.json({ message: "Ошибка при создании пользователя." });
+  }
 };
 //login
 export const login = async (req, res) => {
-    try {
-    } catch (e) {}
+  try {
+  } catch (e) {}
 };
 //get me
 export const getMe = async (req, res) => {
-    try {
-    } catch (e) {}
+  try {
+  } catch (e) {}
 };
