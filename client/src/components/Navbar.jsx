79ed0e596180c7ba6958d310295524e892f0b782
3rdKosme <96114647+3rdKosme@users.noreply.github.com>
2023-06-29 22:58:30 +0300

    small fixes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,31 +1,40 @@
-import React from "react";
+import React from 'react';
 
 export const Navbar = () => {
-  return (
-    <div className="flex py-4 justify-between items-center">
-      <span className="flex justify-center w-6 h-6 bg-gray-600 text-xs text-white rounded-sm">
-        E
-      </span>
-      <ul className="flex gap-8">
-        <li>
-          <a href="/" className="text-sx text-gray-400 hover:text-white">
-            Главная
-          </a>
-        </li>
-        <li>
-          <a href="/" className="text-sx text-gray-400 hover:text-white">
-            Мои посты
-          </a>
-        </li>
-        <li>
-          <a href="/" className="text-sx text-gray-400 hover:text-white">
-            Добавить пост
-          </a>
-        </li>
-      </ul>
-      <div className="flex justify-center items-center bg-gray-600 text-xs text-white rounded-sm px-4">
-        <button>Войти</button>
-      </div>
-    </div>
-  );
+    return (
+        <div className="flex py-4 justify-between items-center">
+            <span className="flex justify-center w-6 h-6 bg-gray-600 text-xs text-white rounded-sm">
+                E
+            </span>
+            <ul className="flex gap-8">
+                <li>
+                    <a
+                        href="/"
+                        className="text-xs text-gray-400 hover:text-white"
+                    >
+                        Главная
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="/"
+                        className="text-xs text-gray-400 hover:text-white"
+                    >
+                        Мои посты
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="/"
+                        className="text-xs text-gray-400 hover:text-white"
+                    >
+                        Добавить пост
+                    </a>
+                </li>
+            </ul>
+            <div className="flex justify-center items-center bg-gray-600 text-xs text-white rounded-sm px-4">
+                <button>Войти</button>
+            </div>
+        </div>
+    );
 };
