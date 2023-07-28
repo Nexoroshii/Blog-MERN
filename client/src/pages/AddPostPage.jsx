2ede13fe7756956da0a4c6be768cf646909ef317
3rdKosme <96114647+3rdKosme@users.noreply.github.com>
2023-07-28 20:07:40 +0300

    add posr page refactor

--- client/src/pages/AddPostPage.jsx
+++ client/src/pages/AddPostPage.jsx
@@ -1,5 +1,46 @@
-import React from "react";
+import React from 'react';
 
 export const AddPostPage = () => {
-  return <div>AddPostPage</div>;
+    return (
+        <form
+            className="w-1/3 mx-auto py-10 "
+            onSubmit={(e) => e.preventDefault()}
+        >
+            <label
+                className="text-gray-300 py-2 bg-gray-600 text-xs mt-2 flex items-center justify-center border-2 border-dotted cursor-pointer
+            "
+            >
+                Прикрепить изображение
+                <input type="file" className="hidden"></input>
+            </label>
+
+            <div className="flex object-cover py-2">IMAGE</div>
+            <label className="text-xs text-white opacity-70">
+                Заголовок поста
+                <input
+                    type="text"
+                    placeholder="Заголовок"
+                    className="mt-1 text-black w-full rounded-lg bg-gray-400 border py-1 px-2 text-xs outline-none placeholder:text-gray-700"
+                ></input>
+            </label>
+
+            <label className="text-xs text-white opacity-70">
+                Текст поста
+                <textarea
+                    type="text"
+                    placeholder="Текст поста"
+                    className="mt-1 text-black w-full rounded-lg bg-gray-400 border py-1 px-2 text-xs outline-none resize-none h-40 placeholder:text-gray-700"
+                ></textarea>
+            </label>
+
+            <div className="flex gap-8 items-center justify-center mt-4">
+                <button className="flex justify-center items-center bg-gray-600 text-xs text-white rounded-sm py-2 px-4">
+                    Добавить пост
+                </button>
+                <button className="flex justify-center items-center bg-red-500 text-xs text-white rounded-sm py-2 px-4">
+                    Отменить пост
+                </button>
+            </div>
+        </form>
+    );
 };
