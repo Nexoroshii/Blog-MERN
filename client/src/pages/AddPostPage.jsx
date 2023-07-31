c166d62c2d1b2610f123fa07f440ce252608ee9f
Egor Work <e.moisei1@ggg.com>
2023-07-31 21:33:05 +0300

    few additions

--- client/src/pages/AddPostPage.jsx
+++ client/src/pages/AddPostPage.jsx
@@ -1,46 +1,65 @@
-import React from 'react';
+import React, { useState } from "react";
 
 export const AddPostPage = () => {
-    return (
-        <form
-            className="w-1/3 mx-auto py-10 "
-            onSubmit={(e) => e.preventDefault()}
-        >
-            <label
-                className="text-gray-300 py-2 bg-gray-600 text-xs mt-2 flex items-center justify-center border-2 border-dotted cursor-pointer
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
+  const [image, setImage] = useState("");
+
+  const submitHandler = () => {
+    try {
+      const data = new FormData();
+      data.append("title", title);
+      data.append("text", text);
+      data.append("image", image);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <form className="w-1/3 mx-auto py-10 " onSubmit={(e) => e.preventDefault()}>
+      <label
+        className="text-gray-300 py-2 bg-gray-600 text-xs mt-2 flex items-center justify-center border-2 border-dotted cursor-pointer
             "
-            >
-                Прикрепить изображение
-                <input type="file" className="hidden"></input>
-            </label>
+      >
+        Прикрепить изображение
+        <input
+          type="file"
+          className="hidden"
+          onChange={(e) => setImage(e.target.files[0])}
+        ></input>
+      </label>
 
-            <div className="flex object-cover py-2">IMAGE</div>
-            <label className="text-xs text-white opacity-70">
-                Заголовок поста
-                <input
-                    type="text"
-                    placeholder="Заголовок"
-                    className="mt-1 text-black w-full rounded-lg bg-gray-400 border py-1 px-2 text-xs outline-none placeholder:text-gray-700"
-                ></input>
-            </label>
+      <div className="flex object-cover py-2">IMAGE</div>
+      <label className="text-xs text-white opacity-70">
+        Заголовок поста
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Заголовок"
+          className="mt-1 text-black w-full rounded-lg bg-gray-400 border py-1 px-2 text-xs outline-none placeholder:text-gray-700"
+        ></input>
+      </label>
 
-            <label className="text-xs text-white opacity-70">
-                Текст поста
-                <textarea
-                    type="text"
-                    placeholder="Текст поста"
-                    className="mt-1 text-black w-full rounded-lg bg-gray-400 border py-1 px-2 text-xs outline-none resize-none h-40 placeholder:text-gray-700"
-                ></textarea>
-            </label>
+      <label className="text-xs text-white opacity-70">
+        Текст поста
+        <textarea
+          onChange={(e) => setText(e.target.value)}
+          type="text"
+          placeholder="Текст поста"
+          className="mt-1 text-black w-full rounded-lg bg-gray-400 border py-1 px-2 text-xs outline-none resize-none h-40 placeholder:text-gray-700"
+        ></textarea>
+      </label>
 
-            <div className="flex gap-8 items-center justify-center mt-4">
-                <button className="flex justify-center items-center bg-gray-600 text-xs text-white rounded-sm py-2 px-4">
-                    Добавить пост
-                </button>
-                <button className="flex justify-center items-center bg-red-500 text-xs text-white rounded-sm py-2 px-4">
-                    Отменить пост
-                </button>
-            </div>
-        </form>
-    );
+      <div className="flex gap-8 items-center justify-center mt-4">
+        <button className="flex justify-center items-center bg-gray-600 text-xs text-white rounded-sm py-2 px-4">
+          Добавить пост
+        </button>
+        <button className="flex justify-center items-center bg-red-500 text-xs text-white rounded-sm py-2 px-4">
+          Отменить пост
+        </button>
+      </div>
+    </form>
+  );
 };
