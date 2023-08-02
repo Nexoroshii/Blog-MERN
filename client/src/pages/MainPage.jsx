56d717bead60d98c6114bfd61a73716517d00e75
3rdKosme <96114647+3rdKosme@users.noreply.github.com>
2023-08-02 23:44:32 +0300

    little additions

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { PostItem } from '../components/PostItem';
 import { PopularPosts } from '../components/PopularPosts';
 import { useDispatch, useSelector } from 'react-redux';
+import { getAllPosts } from '../redux/features/post/postSlice';
 
 export const MainPage = () => {
     const dispatch = useDispatch();
     const { posts, popularPosts } = useSelector((state) => state.post);
+    useEffect(() => {
+        dispatch(getAllPosts());
+    }, [dispatch]);
+
+    if (!posts.length) {
+        return (
+            <div className="text-xl text-center text-white py-10">
+                Постов не существует
+            </div>
+        );
+    }
 
     return (
         <div className="max-w-[900px] mx-auto py-10">
